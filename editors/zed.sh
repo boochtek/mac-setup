52e0b11f11cddde655b4c034882a699e5f95080d
Craig Buchek <craig@boochtek.com>
2026-01-15 13:55:09 -0800

    Zed: Install tools needed by extensions
    Also make sure the binary is in the $PATH.

--- editors/zed.sh
+++ editors/zed.sh
@@ -10,3 +10,20 @@ trap 'RC=$? ; echo "$0: Error on line "$LINENO": $BASH_COMMAND" ; exit $RC' ERR
 brew install --quiet --cask --no-quarantine zed
 
 dockutil --add  '/Applications/Zed.app' --replacing 'Zed' --after 'Visual Studio Code' &> /dev/null
+
+# Install the Zed CLI into our path.
+ln -sf /Applications/Zed.app/Contents/MacOS/zed /usr/local/bin/ 2>/dev/null || \
+    mkdir -p "$HOME/.local/bin/" && ln -sf /Applications/Zed.app/Contents/MacOS/zed "$HOME/.local/bin/"
+
+# Install some tools used by Zed extensions.
+npm install -g cspell@latest
+npm install -g @cspell/cspell-bundled-dicts@latest
+
+brew install --quiet alesbrelih/gitlab-ci-ls/gitlab-ci-ls
+
+npm install -g markdownlint-lsp
+
+brew install --quiet marksman
+brew install --quiet markdown-oxide
+
+brew install --quiet ast-grep
