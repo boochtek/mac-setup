2930c6f526dfb0fd007dfa3d29fc6672ecb10344
Craig Buchek <craig@boochtek.com>
2026-03-18 17:12:29 -0700

    Install usql, a modern universal DB client, inspired by psql

---
+++ dev/db.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+## Databases
+
+# Usql is a modern "universal" DB CLI/REPL inspired by `psql`.
+# It has drivers for PostgreSQL, MySQL, SQLite, SQL Server, Oracle, and CSV.
+brew install --quiet xo/xo/usql
